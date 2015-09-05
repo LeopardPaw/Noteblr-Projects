6d357089307753030a81e53627e781c0f2ff9aba
LeopardPaw <ctadamson41@gmail.com>
2015-09-05 13:28:23 -0500

    Set up

--- ContributionChecker/ContributionChecker.java
+++ ContributionChecker/ContributionChecker.java
@@ -1,7 +1,22 @@
 package com.noteblr.projects;
-
+import com.tumblr.jumblr.*;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
 class ContributionChecker
 {
+	private List<Blog> likeContributors; //used to keep track of contributors
+	private GregorianCalendar calendar; //used to handle timing requests (eg contibutor of the month) 
+	public ContributionChecker()
+	{
+		GregorianCalendar calendar = new GregorianCalendar(new TimeZone("US/CENTRAL"));
+		contributors = 
+	}
+	public String getBiggestContributorbyLikes()
+	{
+		
+	}
 	public static void main(String[] args)
-	{}
+	{
+		
+	}	
 }
